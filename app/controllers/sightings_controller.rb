bfdc7ebec252fdc1faec53b82ef172e3045e745a
Abdul Khan <akhan17@outlook.com>
2020-10-22 18:35:20 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,11 +1,11 @@
 class SightingsController < ApplicationController
-  def index
-    sightings = Sighting.all
-    render json: sightings.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
-  end
+    def index
+      sightings = Sighting.all
+      render json: sightings.to_json(:include => {
+        :bird => {:only => [:name, :species]},
+        :location => {:only => [:latitude, :longitude]}
+      }, :except => [:updated_at])
+    end
 
     def show
         @sighting = Sighting.find(params[:id])
